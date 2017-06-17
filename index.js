630e7d9653aa4367e9f2df54e489fd9ea7bc61d2
thibaultboursier <boursier.thibault@gmail.com>
2017-06-17 19:24:09 +0200

    adding class structure and implementing property binding

--- index.js
+++ index.js
@@ -1,64 +1,139 @@
 'use strict';
 
+class Bindy {}
+Bindy.bind = (...args) => {
+    const core = new Core(...args);
+    core.init();
+};
+
+class Core {
+    constructor(target, DOM) {
+        if (!target) {
+            throw new Error('You must provide an object for binding.');
+        }
+
+        if (!DOM) {
+            throw new Error('You must provide an HTML element for binding.');
+        }
+
+        this.target = target;
+        this.DOM = DOM;
+        this.temp = {};
+        this.bindings = {};
+    }
+
+    init() {
+        this.DOMList = Array.from(this.DOM.querySelectorAll('[bind]'));
+        this.DOMList
+            .map((el) => this.register(el))
+            .forEach((el) => this.add(el));
+
+        if (this.temp.length !== 0) {
+            this.watch();
+        }
+    }
+
+    register(el) {
+        const property = el.getAttribute('bind');
+        this.bindings[property] = el;
+        return property;
+    }
+
+    add(property) {
+        const binding = this.getBinding(property);
+
+        if (!binding.val) {
+            return this.temp[property] = null;
+        }
+
+        this.bind(binding);
+    }
+
+    addNewProperty({
+        obj,
+        key,
+        val,
+        property
+    }) {
+        delete this.temp[property];
+        this.bind({
+            obj,
+            key,
+            val,
+            property
+        });
+        this.updateDOM({
+            property,
+            val
+        });
+    }
+
+    bind({
+        obj,
+        key,
+        val,
+        property
+    }) {
+        Object.defineProperty(obj || this.target, key, {
+            enumerable: true,
+            set: (val) => this.updateDOM({
+                property,
+                val
+            })
+        })
+    }
+
+    updateDOM({
+        property,
+        val
+    }) {
+        const el = this.bindings[property];
+        el.innerText = val;
+    }
+
+    getBinding(property) {
+        const parts = property.split('.');
+        const {
+            length
+        } = parts;
+        let obj;
+        let key;
+
+        let val = parts.reduce((prev, curr, index) => {
+            switch (index) {
+                case length - 1:
+                    key = curr;
+                    break;
+                case length - 2:
+                    obj = prev[curr];
+                    break;
+            }
+
+            return prev ? prev[curr] : undefined;
+        }, scope);
+
+        return {
+            obj,
+            key,
+            val,
+            property
+        };
+    }
+
+    watch() {
+        setInterval(() => {
+            for (let property in this.temp) {
+                if (!this.temp.hasOwnProperty(property)) return;
+
+                const binding = this.getBinding(property);
+
+                if (binding.val) {
+                    this.addNewProperty(binding);
+                }
+            }
+        }, 200);
+    }
+}
+
 let scope = {};
-let bindings = {};
-let temp = [];
-let els;
-
-init(scope, document.body);
-
-function init(target, DOM) {
-    els = Array.from(DOM.querySelectorAll('[bind]'));
-
-    els.map(register)
-        .forEach(add);
-
-    if (temp.length !== 0) {
-        watch();
-    }
-}
-
-function register(el) {
-    const property = el.getAttribute('bind');
-    bindings[property] = el;
-    return property;
-}
-
-function add(property) {
-    if (!isDeclared) {
-        return temp.push(property);
-    }
-
-    bind(property);
-}
-
-function addNewProperty(property) {
-    const val = scope[property];
-
-    temp.splice(temp.indexOf(property), 1);
-    bind(property);
-    updateDOM({ property, val });
-}
-
-function bind(property) {
-    Object.defineProperty(scope, property, {
-        enumerable: true,
-        set: (val) => updateDOM({ property, val })
-    })
-}
-
-function updateDOM({ property, val }) {
-    const el = bindings[property];
-    el.innerText = val;
-}
-
-function isDeclared(property) {
-    return scope.hasOwnProperty(property);
-}
-
-function watch() {
-    setInterval(() => {
-        temp.filter(isDeclared)
-            .forEach(addNewProperty);
-    }, 200);
-}
+const bindy = Bindy.bind(scope, document.body);
