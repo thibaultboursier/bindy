a41a9672811a6f929ad24ee10816b0d7389299f7
thibaultboursier <boursier.thibault@gmail.com>
2017-06-19 01:04:45 +0200

    using RegExp to find bindings and traversing DOM, adding refresh method

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ class Bindy {}
 
 Bindy.bind = (...args) => {
     const view = new View(...args);
+
     view.init();
+
+    return view;
 };
 
 /**
@@ -123,36 +126,137 @@ class View {
      */
     constructor(target, DOM) {
         if (!target) {
-            throw new Error('You must provide an object for binding.');
+            error('You must provide an object for binding.');
         }
 
         if (!DOM) {
-            throw new Error('You must provide an HTML element for binding.');
+            error('You must provide an HTML element for binding.');
         }
 
         this.prefix = 'bd';
         this.target = target;
         this.DOM = DOM;
         this.bindings = [];
+        this.binders = {
+            text(el, value) {
+                const keypath = value;
+
+                this.register({
+                    el,
+                    keypath
+                })
+            }
+        };
     }
 
     /**
      * Initialize view.
      */
     init() {
-        this.DOMList = Array.from(this.DOM.querySelectorAll(`[${this.prefix}-bind]`));
-        this.DOMList
-            .map((el) => this.register(el))
-            .forEach((binding) => this.bind(binding));
+        // DOM is parsed to look for bindings.
+        this.traverseDOM();
+        // Each binding is initialized.
+        this.bindings.forEach((binding) => this.bind(binding));
     }
 
     /**
-     * Register HTMLElement and create new binding.
-     * @param {Object} el - HTML element.
+     * Add new binder.
+     * @param {String} key - Binder's key.
+     * @param {Function} fn - Binder's function.
      * @return {Object}
      */
-    register(el) {
-        const keypath = el.getAttribute(`${this.prefix}-bind`);
+    addBinder(key, fn) {
+        if (binders.hasOwnProperty(key)) {
+            error(`Binding [${key}] is already registered.`);
+        }
+
+        this.binders[key] = fn;
+
+        return this;
+    }
+
+    /**
+     * Refresh.
+     */
+    refresh() {
+        this.bindings.length = 0;
+
+        return this.init();
+    }
+
+    /**
+     * For-loop on each DOM node to look for bindings.
+     * @return {Object}
+     */
+    traverseDOM() {
+        this.getChildNodes(this.DOM);
+
+        return this;
+    }
+
+    /**
+     * Get child nodes.
+     * @param {Object} node - Node.
+     */
+    getChildNodes(node) {
+        const bindingRegExp = this.getBindingRegExp();
+        const {
+            childNodes,
+            nodeType,
+            attributes
+        } = node;
+        let results;
+
+        switch (nodeType) {
+            case 1:
+                results = this.parseAttributes(attributes);
+                break;
+            case 3:
+                // Databinding with delimiters is not supported for now.
+                break;
+        }
+
+        results && results.forEach(({
+            binder,
+            value
+        }) => this.binders[binder].call(this, node, value));
+
+        // Call recursively getChildNodes method.
+        childNodes.forEach(this.getChildNodes.bind(this));
+    }
+
+    /**
+     * Parse attributes to find binders.
+     */
+    parseAttributes(attributes) {
+        const bindingRegExp = this.getBindingRegExp();
+
+        return Array.from(attributes)
+            .filter(({
+                name
+            }) => bindingRegExp.test(name))
+            .map(({
+                value,
+                name
+            }) => {
+                const binder = name.replace(bindingRegExp, '');
+
+                return {
+                    binder,
+                    value
+                }
+            });
+    }
+
+    /**
+     * Register new binding.
+     * @param {Object}
+     * @return {Object}
+     */
+    register({
+        el,
+        keypath
+    }) {
         const {
             target
         } = this;
@@ -176,6 +280,14 @@ class View {
             .parseKeypath()
             .bind();
     }
+
+    /**
+     * Get binding RegExp.
+     * @return {String}
+     */
+    getBindingRegExp() {
+        return new RegExp(`${this.prefix}-`);
+    };
 }
 
 function error(message) {
